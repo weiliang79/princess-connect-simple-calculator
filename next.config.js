37499601b5edde26b7e47aa4732366335c73ef32
weiliang79 <liang2010@hotmail.my>
2023-05-29 14:15:44 +0800

    feat: add csv-loader to config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+      webpack: (config, options) => {
+            config.module.rules.push({
+                  test: /\.csv$/,
+                  loader: 'csv-loader',
+                  options: {
+                        dynamicTyping: true,
+                        header: true,
+                        skipEmptyLines: true
+                  }
+            });
+
+            return config;
+      }
+}
